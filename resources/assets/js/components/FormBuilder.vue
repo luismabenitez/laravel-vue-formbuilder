8eb5c1584ef5119e7ad7152e762ccf49a811cb78
jringeisen <ringeisen85@gmail.com>
2020-11-29 21:05:55 -0500

    Removed unnessary packages. Updated to laravel 7.0 Removed jQuery, bootstrap, and popper. Added TailwindCss. Started replacing bootstrap styling with tailwind styling.

--- resources/assets/js/components/FormBuilder.vue
+++ resources/assets/js/components/FormBuilder.vue
@@ -1,14 +1,15 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6 p-4" style="height:auto;background-color:#ffffff;">
-                <h4 class="text-muted text-center mt-5" v-if="!form.length">DRAG FIELDS HERE</h4>
+    <div class="max-w-2xl mx-auto mt-20">
+        <h1 class="text-center pb-6 text-2xl text-gray-800 font-semibold">Vue FormBuilder</h1>
+        <div class="flex mt-10">
+            <div class="bg-white h-auto rounded-lg shadow-lg p-3 w-2/3 mr-3">
+                <h4 class="text-muted text-center mt-5" v-if="!form.length">CLICK A FIELD TO ADD IT HERE</h4>
                 <form>
-                    <div class='form-group' v-for="(field, index) in form">
-                        <label class="control-label">{{ field.label }}</label><span class="asterisk" v-if="field.required">*</span>
+                    <div class='mt-4' v-for="(field, index) in form" :key="index">
+                        <label class="text-sm font-medium text-gray-700">{{ field.label }}</label><span class="asterisk" v-if="field.required">*</span>
                         <span class="float-right">
-                            <a class="pr-3 field-tools" data-toggle='collapse' :href='"#" + index' role='button' aria-expanded='false'><i class="fas fa-edit"></i></a>
-                            <a class="field-tools" href="#" role='button' @click.prevent="clearField(field, index)"><i class="fas fa-trash"></i></a>
+                            <a class="pr-3 text-xs text-gray-400" @click.prevent="toggleMenu(index)" role='button' aria-expanded='false'><i class="fas fa-edit"></i></a>
+                            <a class="text-xs text-gray-400" href="#" role='button' @click.prevent="clearField(field, index)"><i class="fas fa-trash"></i></a>
                         </span>
                         <template v-if="field.input === 'h2'">
                             <h2>{{ field.text }}</h2>
@@ -17,7 +18,7 @@
                             <p>{{ field.textarea }}</p>
                         </template>
                         <template v-if="field.input === 'input'">
-                            <input :type="field.type" :name="field.name" :class="field.class" :placeholder="field.placeholder" :required="field.required">
+                            <input :type="field.type" :name="field.name" :class="field.class" :placeholder="field.placeholder" :required="field.required" :autocomplete="field.autocomplete">
                         </template>
                         <template v-if="field.input === 'textarea'">
                             <textarea :name="field.name" :rows="field.rows" :class="field.class" :placeholder="field.placeholder" :required="field.required"></textarea>
@@ -25,45 +26,45 @@
                         <template v-if="field.input === 'button'">
                             <button :type="field.name" :class="field.class">{{ field.text }}</button>
                         </template>
-                        <div class='collapse multi-collapse' :id='index'>
+                        <div v-if="toggleMenuId === index">
                             <div class='card card-body'>
-                                <div class="form-group" v-if="field.textarea">
+                                <div class="mt-2" v-if="field.textarea">
                                     <label class="f10">Paragraph Text</label>
-                                    <textarea class="form-control" v-model='field.textarea'>{{ field.textarea }}</textarea>
+                                    <textarea class="py-1 px-4 w-full border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300" v-model='field.textarea'></textarea>
                                 </div>
                                 <div class="form-row">
-                                    <div class="form-group col-md-6" v-if="field.label">
+                                    <div class="mt-2 col-md-6" v-if="field.label">
                                         <label class="f10">Input Label</label>
-                                        <input type='text' class='form-control form-control-sm' placeholder='Enter label here'  v-model='field.label' >
+                                        <input type='text' class='py-1 px-4 w-full border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300' placeholder='Enter label here'  v-model='field.label' >
                                     </div>
-                                    <div class="form-group col-md-6" v-if="field.placeholder">
+                                    <div class="mt-2 col-md-6" v-if="field.placeholder">
                                         <label class="f10">Input Placeholder</label>
-                                        <input type='text' class='form-control form-control-sm' placeholder='Enter placeholder here' v-model='field.placeholder' >
+                                        <input type='text' class='py-1 px-4 w-full border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300' placeholder='Enter placeholder here' v-model='field.placeholder' >
                                     </div>
                                 </div>
                                 <div class="form-row">
-                                    <div class="form-group col-md-4" v-if="field.type">
-                                        <label class="f10">Input Type <span id="example" class="pointer blue" v-on:mouseover="helper" data-toggle="inputType">?</span></label>
-                                        <input type='text' class='form-control form-control-sm' placeholder='Enter type here' v-model='field.type' >
+                                    <div class="mt-2 col-md-4" v-if="field.type">
+                                        <label class="f10">Input Type</label>
+                                        <input type='text' class='py-1 px-4 w-full border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300' placeholder='Enter type here' v-model='field.type' >
                                     </div>
-                                    <div class="form-group col-md-4" v-if="field.rows">
+                                    <div class="mt-2 col-md-4" v-if="field.rows">
                                         <label class="f10">Rows</label>
-                                        <input type='text' class='form-control form-control-sm' placeholder='Enter rows here' v-model='field.rows' >
+                                        <input type='text' class='py-1 px-4 w-full border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300' placeholder='Enter rows here' v-model='field.rows' >
                                     </div>
-                                    <div class="form-group col-md-4" v-if="field.text">
+                                    <div class="mt-2 col-md-4" v-if="field.text">
                                         <label class="f10">Text</label>
-                                        <input type='text' class='form-control form-control-sm' placeholder='Enter text here' v-model='field.text' >
+                                        <input type='text' class='py-1 px-4 w-full border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300' placeholder='Enter text here' v-model='field.text' >
                                     </div>
-                                    <div class="form-group col-md-4" v-if="field.name">
-                                        <label class="f10">Input Name <span id="example" class="pointer blue" v-on:mouseover="helper" data-toggle="inputName">?</span></label>
-                                        <input type='text' class='form-control form-control-sm' placeholder='Enter name here' v-model='field.name' >
+                                    <div class="mt-2 col-md-4" v-if="field.name">
+                                        <label class="f10">Input Name</label>
+                                        <input type='text' class='py-1 px-4 w-full border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300' placeholder='Enter name here' v-model='field.name' >
                                     </div>
-                                    <div class="form-group col-md-4" v-if="field.class">
+                                    <div class="mt-2 col-md-4" v-if="field.class">
                                         <label class="f10">Input Class</label>
-                                        <input type='text' class='form-control form-control-sm' placeholder='Enter class here' v-model='field.class' >
+                                        <input type='text' class='py-1 px-4 w-full border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300' placeholder='Enter class here' v-model='field.class' >
                                     </div>
                                 </div>
-                                <div class="form-group form-check" v-if="field.requiredField">
+                                <div class="mt-2 form-check" v-if="field.requiredField">
                                     <input type="checkbox" class="form-check-input" id="required" v-model="field.required">
                                     <label class="form-check-label f10" for="required">{{ field.requiredField }}</label>
                                 </div>
@@ -72,15 +73,15 @@
                     </div>
                 </form>
             </div>
-            <div class="col-md-4">
-                <ul class="list-group">
-                    <li class="list-group-item pointer" @click="headerField"><i class="fas fa-arrow-left"></i> <span class="pl-5"><i class="fas fa-heading"></i> Header Field</span></li>
-                    <li class="list-group-item pointer" @click="paragraphField"><i class="fas fa-arrow-left"></i> <span class="pl-5"><i class="fas fa-paragraph"></i> Paragraph Field</span></li>
-                    <li class="list-group-item pointer" @click="textField"><i class="fas fa-arrow-left"></i> <span class="pl-5"><i class="fas fa-i-cursor"></i> Text Field</span></li>
-                    <li class="list-group-item pointer" @click="textareaField"><i class="fas fa-arrow-left"></i> <span class="pl-5"><i class="fas fa-align-center"></i> Textarea Field</span></li>
-                    <li class="list-group-item pointer" @click="submitButton"><i class="fas fa-arrow-left"></i> <span class="pl-5"><button class="btn btn-secondary btn-sm"></button> Button</span></li>
+            <div class="flex flex-col w-1/3">
+                <ul class="rounded-lg overflow-hidden shadow">
+                    <li class="bg-white px-2 py-2 shadow cursor-pointer" @click="headerField"><i class="fas fa-arrow-left"></i> <span class="pl-5"><i class="fas fa-heading"></i> Header Field</span></li>
+                    <li class="bg-white px-2 py-2 shadow cursor-pointer" @click="paragraphField"><i class="fas fa-arrow-left"></i> <span class="pl-5"><i class="fas fa-paragraph"></i> Paragraph Field</span></li>
+                    <li class="bg-white px-2 py-2 shadow cursor-pointer" @click="textField"><i class="fas fa-arrow-left"></i> <span class="pl-5"><i class="fas fa-i-cursor"></i> Text Field</span></li>
+                    <li class="bg-white px-2 py-2 shadow cursor-pointer" @click="textareaField"><i class="fas fa-arrow-left"></i> <span class="pl-5"><i class="fas fa-align-center"></i> Textarea Field</span></li>
+                    <li class="bg-white px-2 py-2 shadow cursor-pointer" @click="submitButton"><i class="fas fa-arrow-left"></i> <span class="pl-5"><button class="btn btn-secondary btn-sm"></button> Button</span></li>
                 </ul>
-                <button class="btn btn-primary btn-sm float-right mt-2" @click.prevent="clearForm"><i class="far fa-times-circle"></i> Clear Form</button>
+                <button class="bg-blue-200 px-2 py-2 rounded-lg shadow mt-2 focus:outline-none hover:bg-blue-300" @click.prevent="clearForm"><i class="far fa-times-circle"></i> Clear Form</button>
             </div>
         </div>
     </div>
@@ -91,6 +92,7 @@
         data () {
             return {
                 form: [],
+                toggleMenuId: ''
             }
         },
         methods: {
@@ -101,9 +103,10 @@
                     placeholder: 'Placeholder',
                     type: 'text',
                     name: 'name',
-                    class: 'form-control',
+                    class: 'py-1 px-4 w-full border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300',
                     requiredField: 'required',
-                    required: false
+                    required: false,
+                    autocomplete: 'off'
                 })
             },
             textareaField () {
@@ -113,7 +116,7 @@
                     placeholder: 'Placeholder',
                     name: 'name',
                     rows: '5',
-                    class: 'form-control',
+                    class: 'py-1 px-4 w-full border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300',
                     requiredField: 'required',
                     required: false
                 })
@@ -122,7 +125,7 @@
                 this.form.push({
                     input: 'button',
                     type: 'submit',
-                    class: 'btn btn-primary',
+                    class: 'bg-blue-200 px-4 py-1 rounded-lg shadow mt-2 focus:outline-none hover:bg-blue-300',
                     text: 'Enter Text'
                 })
             },
@@ -139,42 +142,20 @@
                     textarea: 'Paragraph'
                 })
             },
+            toggleMenu (index) {
+                if (!this.toggleMenuId) {
+                    this.toggleMenuId = index
+                } else {
+                    this.toggleMenuId = ''
+                }
+
+            },
             clearField (field, index) {
                 this.form.splice(index, 1);
             },
             clearForm () {
                 this.form = []
-            },
-            helper () {
-                $('[data-toggle="inputType"]').popover({
-                    title: 'Types to Choose:',
-                    content: 'text, email, number, tel',
-                    placement: 'left'
-                })
-                $('[data-toggle="inputName"]').popover({
-                    title: 'Notes',
-                    content: 'This field needs to be specific to a database column',
-                    placement: 'bottom'
-                })
             }
         }
     }
 </script>
-
-<style>
-    .pointer {
-        cursor: pointer;
-    }
-    .f10 {
-        font-size:10px;
-    }
-    .field-tools {
-        color: grey;
-    }
-    .blue {
-        color: #007bff;
-    }
-    .asterisk {
-        color: red;
-    }
-</style>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,22 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
         <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <script src="{{ asset('js/app.js') }}" defer></script>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+        <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+    <body class="bg-gray-100">
+        <div id="app" class="flex-center position-ref full-height">
+            <form-builder></form-builder>
         </div>
     </body>
 </html>
